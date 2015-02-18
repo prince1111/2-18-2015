6a09383cda43fde5f30e4e4c0e8f4381106c8a14
class <class@CST_N417_03.cst.citytech.cuny.edu>
2015-02-18 17:52:09 -0500

    write variable that declares the following.

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,7 +16,23 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        // write variable that declares the following:
+        
+        String name;
+        int age;
+        double annualPay;
+        
+        name = "Anu";
+        
+        age = 22 ;
+        annualPay = 10000; 
+        
+        System.out.println("My name is "  + name  +  ", my age is " + 
+        age + " and I hope to earn  " + annualPay + "  per year ");
+        
+      
+        
+        
+    } 
     
 }
